3e2bfce52c50eb8781b50bbf0be59ced090219d5
shaowei <shaowei@infimind.com>
2019-08-16 21:58:33 +0800

    new

--- proxy/first_proxy.py
+++ proxy/first_proxy.py
@@ -6,9 +6,15 @@ proxies = {
     "https": "socks5h://127.0.0.1:1080",
 }
 
+proxies = {}
 
-def download_file(url):
-    local_filename = url.split('/')[-1]
+
+# bar
+
+def download_file(url, local_filename=None):
+    if local_filename is None:
+        local_filename = url.split('/')[-1]
+        print(local_filename)
     # NOTE the stream=True parameter below
     with requests.get(url, stream=True, proxies=proxies) as r:
         r.raise_for_status()
@@ -20,4 +26,35 @@ def download_file(url):
     return local_filename
 
 
-download_file('https://www.google.com')
+#
+# try:
+#     for index, i in enumerate(list(range(4, 23)) + ["23E24"]):
+#         print("start download the {} movie".format(i))
+#         if isinstance(i, int) and i < 10:
+#             i = '0' + str(i)
+#         download_file(
+#             'http://tj-download.weiyun.com/%E7%A0%B4%E4%BA%A7%E5%A7%90%E5%A6%B9S01E{}.mp4?ver=6100&rkey=2cc2b96d0877cd360541e809490b6f9b5f7d814a522e35d55dbaf79390b0502b83810f5ec5acc8e0623a3819bfa61e20e4f93d536c3128013ae3bcc4719e5476'.format(
+#                 i), '/home/www/part3/Video/破产姐妹S01E{}.mp4'.format(i))
+#         print("end download the {} movie".format(i))
+#
+# except Exception as e:
+#     print(e)
+#     print("failed download the {} movie".format(i))
+# download_file()
+
+for i in """20190703-AI标签.pdf
+data.xml
+package(1).json
+process.png
+Untitled.csv
+update.json
+WechatIMG48.png
+WechatIMG49.png
+产品更新规则schame.txt
+修改商品的传参
+入参(4).txt
+商品发布xml.xml
+天猫发布接口问题-青木txt文件.rar
+""".split('\n'):
+    tmp = i.strip()
+    # download_file('http://192.168.15.33:8000/{}'.format(tmp))
--- test_itchat.py
+++ test_itchat.py
@@ -1,11 +1,189 @@
-import itchat
-
-itchat.auto_login()
-
-itchat.send('Hello, filehelper', toUserName='filehelper')
+# import itchat
+#
+# itchat.auto_login()
+#
+# itchat.send('Hello, filehelper', toUserName='filehelper')
 
 """
 pip install pyqrcode
 pip install pypng
 
 """
+
+import itchat, time
+import matplotlib.pyplot as plot
+import matplotlib.font_manager
+# from matplotlib.font_manager import fontManager as font_manager
+
+# zhfont1 = matplotlib.font_manager.FontProperties(fname='/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf')
+zhfont1 = matplotlib.font_manager.FontProperties(fname='/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc')
+fontP = matplotlib.font_manager.FontProperties()
+fontP.set_family('SimHei')
+fontP.set_size(14)
+
+def chatProportion(rName):
+    itchat.auto_login(True)
+    male = female = other = 0
+    # rName = rName
+    roomSum = 0
+    chatRooms = itchat.search_chatrooms(name=rName)
+    print(chatRooms)
+    if chatRooms is None:
+        print("no this:" + rName)
+    else:
+        chatRoom = itchat.update_chatroom(chatRooms[0]['UserName'], detailedMember=True)
+        index = 0
+        mem_list = chatRoom['MemberList']
+        roomSum = len(mem_list)
+        for friend in mem_list:
+            dis = friend['DisplayName']
+            nick = friend['NickName']
+            sex = friend['Sex']
+            if sex == 1:
+                male += 1
+            elif sex == 2:
+                female += 1
+                print(index, dis, nick, sex)
+            else:
+                other += 1
+            index += 1
+
+    labels = ['男:' + str(male), '女' + str(female), '其他' + str(other)]
+    sizes = [male, female, other]
+    colors = ['green', 'red', 'gray']
+    # 几个分量向外偏移
+    explode = (0.1, 0.1, 0)
+    plot.pie(sizes, explode, labels, colors, '%2.0f%%')
+    plot.axis('equal')
+    plot.legend(loc='upper left', bbox_to_anchor=(-0.1, 1))
+    plot.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+    plot.rcParams['axes.unicode_minus'] = False
+    plot.title("群名：" + str(rName) + "[总人数：" + str(roomSum) + "]\n" + str("(男女比例分析-流年master出品)"))
+    plot.legend(prop=zhfont1)
+    plot.grid()
+    plot.show()
+
+
+import itchat, time
+import matplotlib.pyplot as plt
+
+
+def getInfo():
+    # 测试数据
+    # info = []
+    # city_list = ["西安","大理","西安","city","西安","西安","","大理"]
+    # sex_list = ["男","女","男"]
+    # for city in city_list:
+
+    #     for sex in sex_list:
+    #         per_info = {
+    #             'city':None,
+    #             'sex':None
+    #         }
+    #         if city == "":
+    #             city = "其他"
+
+    #         per_info['city'] = city
+    #         per_info['sex'] = sex
+    #         info.append(per_info)
+
+    # return info
+
+    data_info = []
+    itchat.auto_login(True)
+    # rName = rName
+    chatRooms = itchat.search_chatrooms(name=rName)
+    if chatRooms is None:
+        print("no this:" + rName)
+    else:
+        chatRoom = itchat.update_chatroom(chatRooms[0]['UserName'], detailedMember=True)
+        mem_list = chatRoom['MemberList']
+        for friend in mem_list:
+            sex = friend['Sex']
+            city = friend['City']
+
+            per_info = {
+                'city': None,
+                'sex': None
+            }
+            if city == "":
+                city = "其他"
+            per_info['city'] = city
+            per_info['sex'] = sex
+            data_info.append(per_info)
+    return data_info
+
+
+def cityData(rName):
+    info = getInfo()
+    city_list = []
+    for ereryOne in info:
+        city_list.append(ereryOne['city'])
+
+    # 归一去重
+    single_list = set(city_list)
+    men_arr = []
+    women_arr = []
+    for single in single_list:
+        men_count = 0
+        women_count = 0
+        for everyOne in info:
+            if everyOne['city'] == single:
+                if everyOne['sex'] == 1 or everyOne['sex'] == "男":
+                    men_count += 1
+                else:
+                    women_count += 1
+        men_arr.append(men_count)
+        women_arr.append(women_count)
+
+    wid = 0.4
+    ax = plt.subplot()
+    x_dir = list(range(len(single_list)))
+    # 第二个柱状图偏移一点
+    x_dir2 = [x_dir[i] + wid for i in range(len(x_dir))]
+    tick_loc = [i + wid / 2 for i in range(len(single_list))]
+
+    # 1 竖向柱状图
+    # label1 = plt.bar(x_dir, men_arr, width=wid, fc='gray')
+    # for rect in label1:
+    #     height = rect.get_height()
+    #     # .-0.1 : 反向移动0.1
+    #     plt.text(rect.get_x()+rect.get_width()/2.-0.1, 1.03*height, "%s" % float(height))
+
+    # label2 = plt.bar(x_dir2, women_arr, width=wid, fc='r')
+    # for rect in label2:
+    #     height = rect.get_height()
+    #     plt.text(rect.get_x()+rect.get_width()/2.-0.1, 1.03*height, "%s" % float(height))
+
+    # # 横向标签位置
+    # ax.set_xticks(tick_loc)
+    # # 横向标签名称
+    # ax.set_xticklabels(single_list)
+    # plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+    # plt.title("男女地区竖向柱状图")
+    # plt.show()
+
+    # 2 横向柱状图
+    label1 = ax.barh(x_dir, men_arr, height=wid, fc='gray')
+    for rect in label1:
+        w = rect.get_width()
+        ax.text(w, rect.get_y() + rect.get_height() / 2, '%d' % int(w), ha='left', va='center')
+
+    label2 = ax.barh(x_dir2, women_arr, height=wid, fc='r')
+    for rect in label2:
+        w = rect.get_width()
+        ax.text(w, rect.get_y() + rect.get_height() / 2, '%d' % int(w), ha='left', va='center')
+    plt.legend(prop=zhfont1)
+    # 纵向标签位置
+    ax.set_yticks(tick_loc)
+    # 纵向标签名称
+    ax.set_yticklabels(single_list)
+    # plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+    plt.title("男女地区横向柱状图")
+    plt.show()
+
+
+if __name__ == "__main__":
+    # chatProportion(rName)
+    rName = 'Python私房菜'
+    cityData(rName)
